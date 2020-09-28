585ad7534fdf45292aaa1614ae55b570bcd8d035
nimaeivazzadeh <nima.eivazzadehkaljahi@yahoo.com>
2020-09-28 22:52:16 +0200

    update for the file modofication

--- Exercise Files/Ch4/ospathutils_start.py
+++ Exercise Files/Ch4/ospathutils_start.py
@@ -14,19 +14,26 @@ def main():
 
 
   # Check for item existence and type
-  print("Item exists: " + str(path.exists("textfile")))
-  print("Item is a file: " + str(path.isfile("textfile")))
-  print("Item is a directory: " + str(path.isdir("textfile")))
+  # print("Item exists: " + str(path.exists("textfile")))
+  # print("Item is a file: " + str(path.isfile("textfile")))
+  # print("Item is a directory: " + str(path.isdir("textfile")))
 
   
   # Work with file paths
+  # print("Item path: " + str(path.realpath("textfile")))
+  # print("Item path and name: "+ str(path.split(path.realpath("textfile"))))
 
   
   # Get the modification time
+  # t = time.ctime(path.getmtime("textfile"))
+  # print(t)
+  # print(datetime.datetime.fromtimestamp(path.getatime("textfile")))
 
   
   # Calculate how long ago the item was modified
-
+  td = datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime("textfile"))
+  print("It has been " + str(td) + " Since the file was modified")
+  print("or, " + str(td.total_seconds())+ " seconds")
 
   
 if __name__ == "__main__":

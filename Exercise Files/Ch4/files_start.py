c5b6ae79a459b4c58a007fe51fc77d4e2c6acfc2
nimaeivazzadeh <nima.eivazzadehkaljahi@yahoo.com>
2020-09-28 21:57:21 +0200

    new commit

--- Exercise Files/Ch4/files_start.py
+++ Exercise Files/Ch4/files_start.py
@@ -4,19 +4,30 @@
 
 def main():  
   # Open a file for writing and create it if it doesn't exist
+  # f = open("textfile", "w+")
 
 
   # Open the file for appending text to the end
-
+  f = open("textfile", "r")
 
   # write some lines of data to the file
+  # for i in range(10):
+  #   f.write("This is a line" + str(i) +"\n")
 
   
   # close the file when done
+  # f.close()
+
 
   
   # Open the file back up and read the contents
+  if f.mode == 'r':
+    # contents = f.read()
+    contents = f.readlines()
+    for i in contents:
+      print(i)
 
     
 if __name__ == "__main__":
+  
   main()
